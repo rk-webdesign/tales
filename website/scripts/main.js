215e858f39b02c69cb72713b3526d56228fce619
rk-webdesign <kensy.webdesign@gmail.com>
2024-03-03 11:57:26 +0100

    chore: testing javascript button

---
+++ website/scripts/main.js
@@ -0,0 +1,19 @@
+// main.js
+
+// JavaScript to toggle the visibility of the dropdown content
+document.querySelector('.dropdown-btn').addEventListener('click', function() {
+    document.querySelector('.dropdown-content').classList.toggle('show');
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-btn')) {
+        var dropdowns = document.getElementsByClassName('dropdown-content');
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
